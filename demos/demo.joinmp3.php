f6738e36cbad133585bd05ca96d5b683ef4580b3
James Heinrich <info@silisoftware.com>
2013-03-14 09:21:27 -0500

    v1.9.5-20130220
    inital github release

---
+++ demos/demo.joinmp3.php
@@ -0,0 +1,102 @@
+<?php
+/////////////////////////////////////////////////////////////////
+/// getID3() by James Heinrich <info@getid3.org>               //
+//  available at http://getid3.sourceforge.net                 //
+//            or http://www.getid3.org                         //
+/////////////////////////////////////////////////////////////////
+//                                                             //
+// /demo/demo.joinmp3.php - part of getID3()                   //
+// Sample script for splicing two or more MP3s together into   //
+// one file. Does not attempt to fix VBR header frames.        //
+// See readme.txt for more details                             //
+//                                                            ///
+/////////////////////////////////////////////////////////////////
+
+
+// sample usage:
+// $FilenameOut   = 'combined.mp3';
+// $FilenamesIn[] = 'file1.mp3';
+// $FilenamesIn[] = 'file2.mp3';
+// $FilenamesIn[] = 'file3.mp3';
+//
+// if (CombineMultipleMP3sTo($FilenameOut, $FilenamesIn)) {
+//     echo 'Successfully copied '.implode(' + ', $FilenamesIn).' to '.$FilenameOut;
+// } else {
+//     echo 'Failed to copy '.implode(' + ', $FilenamesIn).' to '.$FilenameOut;
+// }
+
+function CombineMultipleMP3sTo($FilenameOut, $FilenamesIn) {
+
+	foreach ($FilenamesIn as $nextinputfilename) {
+		if (!is_readable($nextinputfilename)) {
+			echo 'Cannot read "'.$nextinputfilename.'"<BR>';
+			return false;
+		}
+	}
+	if (!is_writeable($FilenameOut)) {
+		echo 'Cannot write "'.$FilenameOut.'"<BR>';
+		return false;
+	}
+
+	require_once('../getid3/getid3.php');
+	ob_start();
+	if ($fp_output = fopen($FilenameOut, 'wb')) {
+
+		ob_end_clean();
+		// Initialize getID3 engine
+		$getID3 = new getID3;
+		foreach ($FilenamesIn as $nextinputfilename) {
+
+			$CurrentFileInfo = $getID3->analyze($nextinputfilename);
+			if ($CurrentFileInfo['fileformat'] == 'mp3') {
+
+				ob_start();
+				if ($fp_source = fopen($nextinputfilename, 'rb')) {
+
+					ob_end_clean();
+					$CurrentOutputPosition = ftell($fp_output);
+
+					// copy audio data from first file
+					fseek($fp_source, $CurrentFileInfo['avdataoffset'], SEEK_SET);
+					while (!feof($fp_source) && (ftell($fp_source) < $CurrentFileInfo['avdataend'])) {
+						fwrite($fp_output, fread($fp_source, 32768));
+					}
+					fclose($fp_source);
+
+					// trim post-audio data (if any) copied from first file that we don't need or want
+					$EndOfFileOffset = $CurrentOutputPosition + ($CurrentFileInfo['avdataend'] - $CurrentFileInfo['avdataoffset']);
+					fseek($fp_output, $EndOfFileOffset, SEEK_SET);
+					ftruncate($fp_output, $EndOfFileOffset);
+
+				} else {
+
+					$errormessage = ob_get_contents();
+					ob_end_clean();
+					echo 'failed to open '.$nextinputfilename.' for reading';
+					fclose($fp_output);
+					return false;
+
+				}
+
+			} else {
+
+				echo $nextinputfilename.' is not MP3 format';
+				fclose($fp_output);
+				return false;
+
+			}
+
+		}
+
+	} else {
+
+		$errormessage = ob_get_contents();
+		ob_end_clean();
+		echo 'failed to open '.$FilenameOut.' for writing';
+		return false;
+
+	}
+
+	fclose($fp_output);
+	return true;
+}
---
+++ demos/demo.simple.write.php
@@ -0,0 +1,59 @@
+<?php
+/////////////////////////////////////////////////////////////////
+/// getID3() by James Heinrich <info@getid3.org>               //
+//  available at http://getid3.sourceforge.net                 //
+//            or http://www.getid3.org                         //
+/////////////////////////////////////////////////////////////////
+//                                                             //
+// /demo/demo.simple.write.php - part of getID3()              //
+// Sample script showing basic syntax for writing tags         //
+// See readme.txt for more details                             //
+//                                                            ///
+/////////////////////////////////////////////////////////////////
+
+//die('Due to a security issue, this demo has been disabled. It can be enabled by removing line '.__LINE__.' in '.$_SERVER['PHP_SELF']);
+
+$TextEncoding = 'UTF-8';
+
+require_once('../getid3/getid3.php');
+// Initialize getID3 engine
+$getID3 = new getID3;
+$getID3->setOption(array('encoding'=>$TextEncoding));
+
+require_once('../getid3/write.php');
+// Initialize getID3 tag-writing module
+$tagwriter = new getid3_writetags;
+//$tagwriter->filename = '/path/to/file.mp3';
+$tagwriter->filename = 'c:/file.mp3';
+
+//$tagwriter->tagformats = array('id3v1', 'id3v2.3');
+$tagwriter->tagformats = array('id3v2.3');
+
+// set various options (optional)
+$tagwriter->overwrite_tags = true;
+$tagwriter->overwrite_tags = false;
+$tagwriter->tag_encoding = $TextEncoding;
+$tagwriter->remove_other_tags = true;
+
+// populate data array
+$TagData = array(
+	'title'         => array('My Song'),
+	'artist'        => array('The Artist'),
+	'album'         => array('Greatest Hits'),
+	'year'          => array('2004'),
+	'genre'         => array('Rock'),
+	'comment'       => array('excellent!'),
+	'track'         => array('04/16'),
+	'popularimeter' => array('email'=>'user@example.net', 'rating'=>128, 'data'=>0),
+);
+$tagwriter->tag_data = $TagData;
+
+// write tags
+if ($tagwriter->WriteTags()) {
+	echo 'Successfully wrote tags<br>';
+	if (!empty($tagwriter->warnings)) {
+		echo 'There were some warnings:<br>'.implode('<br><br>', $tagwriter->warnings);
+	}
+} else {
+	echo 'Failed to write tags!<br>'.implode('<br><br>', $tagwriter->errors);
+}
