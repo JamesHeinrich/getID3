55ab5a06b563555fde53e4afa8a35c8bbc026579
James Heinrich <info@silisoftware.com>
2013-09-10 03:33:22 -0400

    ID3v2 possible undefined comment index

--- getid3/module.tag.id3v2.php
+++ getid3/module.tag.id3v2.php
@@ -1082,7 +1082,7 @@ class getid3_id3v2 extends getid3_handler
 				$parsedFrame['description']  = $frame_description;
 				$parsedFrame['data']         = $frame_text;
 				if (!empty($parsedFrame['framenameshort']) && !empty($parsedFrame['data'])) {
-					$commentkey = ($parsedFrame['description'] ? $parsedFrame['description'] : count($info['id3v2']['comments'][$parsedFrame['framenameshort']]));
+					$commentkey = ($parsedFrame['description'] ? $parsedFrame['description'] : (!empty($info['id3v2']['comments'][$parsedFrame['framenameshort']]) ? count($info['id3v2']['comments'][$parsedFrame['framenameshort']]) : 0));
 					if (!isset($info['id3v2']['comments'][$parsedFrame['framenameshort']]) || !array_key_exists($commentkey, $info['id3v2']['comments'][$parsedFrame['framenameshort']])) {
 						$info['id3v2']['comments'][$parsedFrame['framenameshort']][$commentkey] = getid3_lib::iconv_fallback($parsedFrame['encoding'], $info['id3v2']['encoding'], $parsedFrame['data']);
 					} else {
