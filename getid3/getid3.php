c74c700c9e5e3996b8c5bbb445c76226d3e1f64f
James Heinrich <info@silisoftware.com>
2015-10-11 12:18:15 -0400

    demo.joinmp3.php enhancements
    function CombineMultipleMP3sTo() in demos/demo.joinmp3.php can now be
    used to extact audio samples from one or more input files.

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -109,7 +109,7 @@ class getID3
 	protected $startup_error   = '';
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.10-20150914';
+	const VERSION           = '1.9.10-201510111213';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
