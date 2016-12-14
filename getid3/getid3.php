b9a8564e56bdcc294dc7ade32a7f67885bed3778
James Heinrich <info@silisoftware.com>
2016-12-14 09:29:29 -0500

    v1.9.13 changelog

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -112,7 +112,7 @@ class getID3
 	protected $startup_error   = '';
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.12-201612051806';
+	const VERSION           = '1.9.13-201612051806';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
