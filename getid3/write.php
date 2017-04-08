e19098ae13dda8f9ae25c8b9aeebd81348aaa9e4
James Heinrich <info@silisoftware.com>
2017-04-08 09:36:31 -0400

    latest-version url

--- getid3/write.php
+++ getid3/write.php
@@ -414,7 +414,7 @@ class getid3_writetags
 		if ($this->overwrite_tags) {
 			// do nothing - ignore previous data
 		} else {
-throw new Exception('$this->overwrite_tags=false is known to be buggy in this version of getID3. Will be fixed in the near future, check www.getid3.org for a newer version.');
+throw new Exception('$this->overwrite_tags=false is known to be buggy in this version of getID3. Check http://github.com/JamesHeinrich/getID3 for a newer version.');
 			if (!isset($this->ThisFileInfo['tags'][$TagFormat])) {
 				return false;
 			}
