34c8eaf2c670a1f319a6721e7ad09558382a28f2
James Heinrich <info@silisoftware.com>
2018-02-04 14:50:10 -0500

    version string update
    Update getID3 version string for changes made in
    https://github.com/JamesHeinrich/getID3/pull/143

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -252,7 +252,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.15-201801240839';
+	const VERSION           = '1.9.15-201802031449';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
