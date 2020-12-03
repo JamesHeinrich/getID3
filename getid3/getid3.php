a331342baab6b59951043f1d79d740ce30ff0ea0
James Heinrich <info@silisoftware.com>
2020-12-03 10:25:53 -0500

    set_error_handler $array_context deprecated
    https://github.com/JamesHeinrich/getID3/issues/275
    https://www.php.net/manual/en/function.set-error-handler.php

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -257,7 +257,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.20-202010211809';
+	const VERSION           = '1.9.20-202012031023';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
