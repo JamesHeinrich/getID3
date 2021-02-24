34a0bb2db147633173bcf5db5e72f5195eba3d3e
James Heinrich <info@silisoftware.com>
2021-02-24 17:26:14 -0500

    isset+>0 to !empty

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -257,7 +257,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.20-202102232229';
+	const VERSION           = '1.9.20-202102241724';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
