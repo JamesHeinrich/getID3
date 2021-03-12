df88a6cead0baffbc000d845e5f75316c3884aa2
James Heinrich <info@silisoftware.com>
2021-03-11 22:23:46 -0500

    misc.iso undefined index
    https://github.com/JamesHeinrich/getID3/issues/297

--- getid3/module.misc.iso.php
+++ getid3/module.misc.iso.php
@@ -53,8 +53,10 @@ class getid3_iso extends getid3_handler
 		$this->ParsePathTable();
 
 		$info['iso']['files'] = array();
-		foreach ($info['iso']['path_table']['directories'] as $directorynum => $directorydata) {
-			$info['iso']['directories'][$directorynum] = $this->ParseDirectoryRecord($directorydata);
+		if (!empty($info['iso']['path_table']['directories'])) {
+			foreach ($info['iso']['path_table']['directories'] as $directorynum => $directorydata) {
+				$info['iso']['directories'][$directorynum] = $this->ParseDirectoryRecord($directorydata);
+			}
 		}
 
 		return true;
