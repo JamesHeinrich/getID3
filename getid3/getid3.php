a99e0a20540259911e95e3bcfdd18db2ba8222dc
James Heinrich <info@silisoftware.com>
2021-12-15 11:11:36 -0500

    #355 undefined index ID3v2 chapters
    https://github.com/JamesHeinrich/getID3/issues/355

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -387,7 +387,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.21-202111211051';
+	const VERSION           = '1.9.21-202112151109';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
