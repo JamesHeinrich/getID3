28ad3d966062aac975ac5919b609aa3df477467c
Víctor García <vgpastor@ingenierosweb.co>
2022-02-21 20:48:19 +0100

    Fix remote urls pattern

--- src/GetID3.php
+++ src/GetID3.php
@@ -432,7 +432,7 @@ class GetID3
 			$this->info['php_memory_limit'] = (($this->memory_limit > 0) ? $this->memory_limit : false);
 
 			// remote files not supported
-			if (preg_match('#^(ht|f)tp://#', $filename)) {
+			if (preg_match('#^(ht|f)tp(s)://#', $filename)) {
 				throw new Exception('Remote files are not supported - please copy the file locally first');
 			}
 
