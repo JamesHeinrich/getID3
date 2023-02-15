dbfce30a8acb1c2962836e4cb80fa197b66a78b6
Brion Vibber <brion@pobox.com>
2023-02-15 09:26:02 -0800

    Fix for MPEG-1 video pixel aspect ratio
    The reciprocal of the actual value was being returned for MPEG-1
    video. Files with square pixels were not affected, nor were MPEG-2
    files listing a display aspect ratio.
    
    Fixes https://github.com/JamesHeinrich/getID3/issues/410

--- getid3/module.audio-video.mpeg.php
+++ getid3/module.audio-video.mpeg.php
@@ -618,6 +618,9 @@ echo 'average_File_bitrate = '.number_format(array_sum($vbr_bitrates) / count($v
 		if ($mpeg_version == 2 && $ratio != 1 && $width != 0) {
 			// Calculate pixel aspect ratio from MPEG-2 display aspect ratio
 			$ratio = $ratio * $height / $width;
+		} else if ($ratio > 0) {
+			// The MPEG-1 tables store the reciprocal of the pixel aspect ratio.
+			$ratio = 1 / $ratio;
 		}
 		return $ratio;
 	}
