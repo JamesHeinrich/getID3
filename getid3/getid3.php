449956e1269bcbdb60a7936222f1923817356779
James Heinrich <info@getid3.org>
2024-03-23 08:55:48 -0400

    APE 32-bit PHP warning
    https://github.com/JamesHeinrich/getID3/issues/439

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -387,7 +387,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.23-202402191012';
+	const VERSION           = '1.9.23-202403230855';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
