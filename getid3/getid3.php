3a59f2e0567cd560d5197638f35bc3b544896a70
James Heinrich <info@getid3.org>
2024-08-08 13:50:21 -0400

    #450 Ogg wrong duration
    https://github.com/JamesHeinrich/getID3/issues/450

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -387,7 +387,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.23-202408061058';
+	const VERSION           = '1.9.23-202408081349';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
