1a83076d7022286b2225596090df983dbe5778e2
James Heinrich <info@getid3.org>
2026-09-21 08:44:50 -0400

    remove LIBXML_NOENT
    https://github.com/JamesHeinrich/getID3/security/advisories/GHSA-j649-xr34-mmmh

--- getid3/getid3.php
+++ getid3/getid3.php
@@ -387,7 +387,7 @@ class getID3
 	 */
 	protected $startup_warning = '';
 
-	const VERSION           = '1.9.26-202609201631';
+	const VERSION           = '1.9.26-202609210845';
 	const FREAD_BUFFER_SIZE = 32768;
 
 	const ATTACHMENTS_NONE   = false;
